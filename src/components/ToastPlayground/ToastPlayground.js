4bd5d9c6f606dcc07e0de4412fd4050ee136d77c
Mario Barceló <mariobarcelosospedra@gmail.com>
2023-06-13 15:47:12 +0200

    Exercise 2: Live-editable toast preview - My Solution

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Button from '../Button';
 
+import Toast from '../Toast';
+
 import styles from './ToastPlayground.module.css';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
@@ -9,6 +11,7 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
 	const [message, setMessage] = React.useState('');
 	const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+	const [isShown, setIsShown] = React.useState(false);
 
 	return (
 		<div className={styles.wrapper}>
@@ -16,6 +19,13 @@ function ToastPlayground() {
 				<img alt='Cute toast mascot' src='/toast.png' />
 				<h1>Toast Playground</h1>
 			</header>
+
+			<Toast
+				isShown={isShown}
+				setIsShown={setIsShown}
+				variant={variant}>
+				{message}
+			</Toast>
 
 			<div className={styles.controlsWrapper}>
 				<div className={styles.row}>
@@ -59,8 +69,6 @@ function ToastPlayground() {
 								</label>
 							);
 						})}
-
-						{/* TODO Other Variant radio buttons here */}
 					</div>
 				</div>
 
@@ -68,7 +76,12 @@ function ToastPlayground() {
 					<div className={styles.label} />
 					<div
 						className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-						<Button>Pop Toast!</Button>
+						<Button
+							onClick={() => {
+								setIsShown(true);
+							}}>
+							Pop Toast!
+						</Button>
 					</div>
 				</div>
 			</div>
