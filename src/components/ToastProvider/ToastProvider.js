f65dcd463cba986148cfb2dfcdb76a9a56ead03c
Mario Barceló <mariobarcelosospedra@gmail.com>
2023-06-14 18:17:17 +0200

    Exercise 6: Extracting a custom hook - My Solution

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,23 +1,13 @@
 import React from 'react';
-
+import useEscapeKey from '../../hooks/use-escape-key.js';
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
 	const [toasts, setToasts] = React.useState([]);
 
-	React.useEffect(() => {
-		function handleDismissAllToasts(event) {
-			if (event.key === 'Escape') {
-				setToasts([]);
-			}
-		}
-
-		window.addEventListener('keydown', handleDismissAllToasts);
-
-		// Cleanup function
-		return () =>
-			window.removeEventListener('keydown', handleDismissAllToasts);
-	}, []);
+	useEscapeKey(() => {
+		setToasts([]);
+	});
 
 	function createToast(message, variant) {
 		const nextToast = { message, variant, id: crypto.randomUUID() };
